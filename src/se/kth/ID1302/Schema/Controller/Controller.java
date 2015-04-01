be0b64679df93b425f8daed041f57f1362828ba4
twentyone <konstantin.sozinov@hotmail.se>
2015-04-01 14:29:04 +0200

    hej

---
+++ src/se/kth/ID1302/Schema/Controller/Controller.java
@@ -0,0 +1,10 @@
+package se.kth.ID1302.Schema.Controller;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/se/kth/ID1302/Schema/View/View.java
@@ -0,0 +1,10 @@
+package se.kth.ID1302.Schema.View;
+
+public class View {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
