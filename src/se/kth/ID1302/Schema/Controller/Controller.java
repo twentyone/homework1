8cc68c17a9cae970d492acff3d519af7a23f1435
Max <swinglea@gmail.com>
2015-04-02 11:32:45 +0200

    Starting with Controller

--- src/se/kth/ID1302/Schema/Controller/Controller.java
+++ src/se/kth/ID1302/Schema/Controller/Controller.java
@@ -1,10 +1,25 @@
 package se.kth.ID1302.Schema.Controller;
 
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Component;
+
 public class Controller {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int OptimizedCalender; //The wished for datastructure of the final result
+	private EventTree CalendarOfTeamMembers; //bag?
+	
+	public void createOptimizedEvent() //Type??
+	{
+		if(this.CalendarOfTeamMembers == null)
+		{
+			System.out.println("Schema ej angivet");
+		}
+		
 	}
-
+	
+	public void OptimizeEvent()
+	{
+		
+	}
+	
 }
